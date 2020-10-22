b882907eba89947c5359fc7e0aafeda23ef4241f
jimin-oh <wlals6828@daum.net>
2020-10-23 08:19:57 +0900

    20%

--- app/src/main/java/com/sacol/sharinggoal/DetailActivity.java
+++ app/src/main/java/com/sacol/sharinggoal/DetailActivity.java
@@ -1,4 +1,46 @@
 package com.sacol.sharinggoal;
 
-public class DetailActivity {
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    private TextView current_goal ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+        init();
+        setUp();
+
+    }
+
+    private void init() {
+        Intent intent  = getIntent();
+        String goal = intent.getExtras().getString("goal");
+        current_goal = findViewById(R.id.current_goal);
+        current_goal.setText(goal);
+    }
+
+    private void setUp() {
+
+    }
+
+
 }
