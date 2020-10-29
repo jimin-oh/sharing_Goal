812517a9dfc9befdbe3a13f3a59a190dd81783d7
jimin-oh <wlals6828@daum.net>
2020-10-29 22:53:18 +0900

    ProfileActivity intent

--- app/src/main/java/com/sacol/sharinggoal/ProfileActivity.java
+++ app/src/main/java/com/sacol/sharinggoal/ProfileActivity.java
@@ -9,29 +9,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-
-public class SettingActivity extends AppCompatActivity {
-    private Button logout_btn;
-    private Button profile_btn;
+public class ProfileActivity extends  AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_profile);
         init();
         setUp();
+
     }
 
     private void init() {
-        logout_btn = findViewById(R.id.logout_btn);
-        profile_btn= findViewById(R.id.profileSetting_btn);
+
 
     }
 
 
     private void setUp() {
-        logout_btn.setOnClickListener(logout);
-        profile_btn.setOnClickListener(gotoProfilePage);
+
     }
 
     View.OnClickListener logout = new View.OnClickListener() {
@@ -42,20 +38,8 @@ public class SettingActivity extends AppCompatActivity {
         }
     };
 
-    View.OnClickListener gotoProfilePage = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startProfileActivity();
-        }
-    };
-
     private void startMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    private void startProfileActivity() {
-        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 }
